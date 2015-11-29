969cc76c273a747680b1280ef9da4bd1209376c1
Andi Schnebinger <andi.schnebinger@googlemail.com>
2015-11-29 17:02:30 +0100

    refactor and style

--- tft/show_stream/show_stream.py
+++ tft/show_stream/show_stream.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from subprocess import *
+from subprocess import Popen
 from time import sleep, strftime
 from datetime import datetime
 import time
@@ -9,15 +9,17 @@ import sys
 import requests
 import logging
 
-logging.basicConfig(filename='show_stream.log',level=logging.DEBUG)
+logging.basicConfig(filename='show_stream.log', level=logging.DEBUG)
 isBroken = True
+
 
 def run_cmd(stream):
     tft_display = 'DISPLAY=:0.0 '
     vlc_command = 'vlc -vvv -f '
-    log = ' 2>&1 > /tmp/vlc.log'
-    # for example "DISPLAY=:0.0 vlc -vvv -f http://141.60.125.254:8080/?action=stream 2>&1 > /tmp/vlc.log"
-    cmd_show = tft_display + vlc_command + stream + log
+    error_log = ' 2>&1 > /tmp/vlc.log'
+    # example: "DISPLAY=:0.0 vlc -vvv -f http://141.60.125.254:8080/?
+    #           action=stream 2>&1 > /tmp/vlc.log"
+    cmd_show = tft_display + vlc_command + stream + error_log
     try:
         p = Popen(cmd_show, shell=True, stdout=PIPE)
         output = p.communicate()[0]
@@ -26,15 +28,16 @@ def run_cmd(stream):
     return output
 
 
-def check_stream(ip,port):
+def check_stream(ip, port):
     http = 'http://'
     action = '/?action=stream'
     stream = http + ip + ':' + port + action
     while(True):
         try:
             # for example 'http://141.60.125.254:8080/?action=stream'
-            r = requests.get(stream  , timeout=0.005)
-        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            r = requests.get(stream, timeout=0.005)
+        except (requests.exceptions.Timeout,
+                requests.exceptions.ConnectionError):
             isBroken = True
             logging.info('Timeout Error: ' + ip)
             sleep(30)
@@ -55,7 +58,7 @@ def check_stream(ip,port):
 def main():
     ip = str(sys.argv[1])
     port = str(sys.argv[2])
-    check_stream(ip,port)
+    check_stream(ip, port)
 
 if __name__ == '__main__':
     main()

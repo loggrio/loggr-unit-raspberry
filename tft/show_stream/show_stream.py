4da989181e94c6c01ccef4994f189b31f65a8def
Jessica Zimpel <jessica-zimpel@gmx.de>
2015-11-30 13:22:28 +0100

    update show stream script

--- tft/show_stream/show_stream.py
+++ tft/show_stream/show_stream.py
@@ -1,58 +1,81 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from subprocess import Popen
+from subprocess import Popen,PIPE
 from time import sleep, strftime
 from datetime import datetime
+import os
 import time
 import sys
 import requests
 import logging
 
 logging.basicConfig(filename='show_stream.log', level=logging.DEBUG)
-isBroken = True
 
 
-def run_cmd(stream):
-    tft_display = 'DISPLAY=:0.0 '
-    vlc_command = 'vlc -vvv -f '
-    error_log = ' 2>&1 > /tmp/vlc.log'
+
+def run_cmd(cmd):
+
     # example: "DISPLAY=:0.0 vlc -vvv -f http://141.60.125.254:8080/?
     #           action=stream 2>&1 > /tmp/vlc.log"
-    cmd_show = tft_display + vlc_command + stream + error_log
+
     try:
-        p = Popen(cmd_show, shell=True, stdout=PIPE)
-        output = p.communicate()[0]
+        resultcode = os.system(cmd)
     except OSError as e:
         logging.info('Start VLC Error: ' + e.output)
-    return output
+    return resultcode
 
 
 def check_stream(ip, port):
     http = 'http://'
     action = '/?action=stream'
     stream = http + ip + ':' + port + action
+    tft_display = 'DISPLAY=:0.0 '
+    vlc_command = 'vlc -vvv -f --play-and-exit '
+    error_log = ' 2>&1 > /tmp/vlc.log'
+    cmd_show = tft_display + vlc_command + stream + error_log
+    kill = 'sudo killall vlc'
+    isBroken = True
+    time = 5
     while(True):
         try:
             # for example 'http://141.60.125.254:8080/?action=stream'
-            r = requests.get(stream, timeout=0.005)
-        except (requests.exceptions.Timeout,
-                requests.exceptions.ConnectionError):
+            print "vor request"
+            r = requests.head('http://141.60.131.238:8080/?action=stream')
+            print r
+            print "nach request"
+        except (requests.exceptions.ConnectionError):
+            print "ConnectionError try again"
             isBroken = True
-            logging.info('Timeout Error: ' + ip)
-            sleep(30)
+            logging.info('Connection Error: ' + ip)
+            sleep(time)
             continue
 
-        if r.status_code != requests.codes.ok:
-            isBroken = True
+
+        if r.status_code == requests.codes.ok and isBroken == True:
+            run_cmd(cmd_show)
+            isBroken = False
+            sleep(time)
+            #debugging
+
+            #r.status_code = 400
+        if r.status_code == 400 and isBroken == True:
+            isBroken = False
+            run_cmd(cmd_show) #debug
             logging.info('Request Code not ok')
-            sleep(30)
+            sleep(time)
             continue
+
+        elif r.status_code != requests.codes.ok:
+            isBroken = True
+            sleep(5)
+
 
         elif isBroken is True:
-            run_cmd(stream)
+            run_cmd(cmd_show)
             logging.info('Start Stream')
             isBroken = False
+
 
 
 def main():
